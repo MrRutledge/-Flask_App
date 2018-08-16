82b84f5cebfbf1732a4b3ba85bef70ad8f2dc9fe
MrRutledge <rahimrutledge@gmail.com>
2018-08-16 23:30:30 +0100

    &*&*&*&*

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,23 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "What up fucker!?!"
+    user = {'username': 'Rutledge'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Kim2'},
+            'body': 'The average movie goer will!'
+        }
+    ]
+    return render_template('index.html', title='Home2', user=user, posts=posts)
+ 
