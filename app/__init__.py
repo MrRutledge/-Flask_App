c558d8a7021918670084bb7d01207609562c6ea9
MrRutledge <rahimrutledge@gmail.com>
2019-01-17 23:25:41 +0000

    Feat: Updated all files to Python 3.7

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
+
+# Where we're importing the packedge form
+
 from flask import Flask
+from flask_debug import Debug
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+Debug(app)
+app.run(debug=True)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,32 +1,28 @@
+
+# Importing the app package using app
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
-
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Rutledge'}
-    posts = [
+     user = {'user': 'MrRutledge'}
+     posts = [
         {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
+            'author': {'username': 'MrRutledge'},
+            'body': 'Beautiful day in Croydon'
         },
         {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        },
-        {
-            'author': {'username': 'Kim2'},
-            'body': 'The average movie goer will!'
+             'author':{'username':'MowHawk'},
+             'body': 'The Titanic movie was so super'
         }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
- 
-@app.route('/login')
+     ]
+     return render_template('index.html',title='Brome', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        flash('Login requested for user {}, remember={}'.format(form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
