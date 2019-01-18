f58df5b512c3cf2dc074d04c355e98512231333a
MrRutledge <rahimrutledge@gmail.com>
2019-01-18 04:10:48 +0000

    Feat: Adding databases to the app

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,19 @@
 
-# Where we're importing the packedge form
+# Where we're importing the packedges form
 
 from flask import Flask
-from flask_debug import Debug
+#from flask_debug import Debug
 from config import Config
+#databases
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-Debug(app)
-app.run(debug=True)
+#Debug(app)
+#app.run(debug=True)
 
-from app import routes
+from app import routes, models
