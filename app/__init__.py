56e6113682573f1a57334c288047ad62e9b14057
MrRutledge <rahimrutledge@gmail.com>
2019-02-05 18:39:08 +0000

    Hash code implementation

--- app/__init__.py
+++ app/__init__.py
@@ -7,11 +7,18 @@ from config import Config
 #databases
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+#flask-login
+from flask_login import LoginManager
+
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+
 
 #Debug(app)
 #app.run(debug=True)
--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,32 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+#part of the user registration
+from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+ 
+ #class to allow users to register on the web
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('I want In!')
+
+# restrict Users from using the same details to registor
+    def validate_username(self, username):
+         user = User.query.filter_by(username=username.data).first()
+         if user is not None:
+             raise ValidationError('Please use a different username')
+     
+    def validate_email(self,email): 
+         user = User.query.filter_by(email=email.data).first()
+         if user is not None:
+             raise ValidationError('Please entre the another email address')
+
+          
